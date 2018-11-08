fc3f59f734a50031ae8070077f094b16eb186a85
priscilla <941705864@qq.com>
2018-11-08 08:29:46 +0800

    class test

--- mypython_1.py
+++ mypython_1.py
@@ -1,9 +1,27 @@
-class LNode:
-	def __new__(self, x):
-		self.data = x
-		self.next = None
+class employee(object):
+	"""docstring for employee"""
+	empcount = 0
+	def __init__(self, name, salary):
+		self.name = name
+		self.salary = salary
+		employee.empcount += 1
 
-def Reverse(head):
-	if head == None or head.next == None:
+	def displaycount(self):
+		print("Total employee:" + str(employee.empcount))
+
+	def printemployee(self):
+		print("name:" + str(self.name) + "salary:" + str(self.salary))
+
+emp1 = employee("Marry", 1000)
+emp2 = employee("David", 2000)
+
+emp1.name = "Peter"
+#change emp1.name into Peter
+emp1.printemployee()
+emp2.printemployee()
+print(employee.empcount)
+
+
+		
 		
 		
